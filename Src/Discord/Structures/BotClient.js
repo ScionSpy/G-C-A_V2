41d68e11e4f0c8cd1eda633c29aa95776733f0ee
§π∆d°≈~§¶¥#1904 <29105063+ScionSpy@users.noreply.github.com>
2024-08-08 19:39:49 -0400

    Startup - console. || PrettyArrays + Error visibility ( ! )
    Errors will later be broadcast to the support server.
      Until then they're through the console.

--- Src/Discord/Structures/BotClient.js
+++ Src/Discord/Structures/BotClient.js
@@ -36,7 +36,7 @@ module.exports = class BotClient extends Client {
      * @param {string} directory directory containing the event files
      */
     loadEvents(directory) {
-        console.log(`Loading events...`);
+        console.log(`> Loading events...`);
         let success = 0;
         let failed = 0;
         const clientEvents = [];
@@ -48,7 +48,8 @@ module.exports = class BotClient extends Client {
                 const event = require(filePath);
 
                 this.on(eventName, event.bind(null, this));
-                clientEvents.push([file, "✓"]);
+                //clientEvents.push(`✓ ${file}`);
+                clientEvents.push(["✓", file]);
 
                 delete require.cache[require.resolve(filePath)];
                 success += 1;
@@ -58,9 +59,10 @@ module.exports = class BotClient extends Client {
             };
         });
 
-        console.log(clientEvents);
+        let events = prettyArrays(clientEvents, 2);
+        console.log(events);
 
-        console.log(`Loaded ${success + failed} events. Success (${success}) Failed (${failed})`);
+        console.log(`>> Loaded ${success + failed} events. Success (${success}) Failed (${failed})\n`);
     };
 
     /**
@@ -100,18 +102,58 @@ module.exports = class BotClient extends Client {
      * @param {string} directory
      */
     loadCommands(directory) {
-        console.log(`Loading commands...`);
+        console.log(`> Loading commands...`);
+        let success = 0;
+        let failed = 0;
+        const clientCommands = [];
+
         const files = recursiveReadDirSync(directory);
         for (const file of files) {
             try {
                 const cmd = require(file);
                 if (typeof cmd !== "object") continue;
                 this.loadCommand(cmd);
+
+                clientCommands.push(`✓ ${cmd.name}`);
+                success += 1;
             } catch (ex) {
-                console.error(`⚠⚠⚠    Failed to load ${file} Reason: ${ex.message}`);
+                failed += 1;
+                console.error(`⚠⚠⚠⚠⚠⚠⚠⚠⚠\n\nFailed to load ${file} Reason: ${ex.message}\n\n⚠⚠⚠⚠⚠⚠⚠⚠⚠`);
             };
         };
 
-        console.log(`Loaded ${this.commands.length} commands`);
+        let cmds = prettyArrays(clientCommands, 5);
+        console.log(cmds);
+
+        console.log(`>> Loaded ${success + failed} events. Success (${success}) Failed (${failed})\n`);
     };
+};
+
+
+/**
+ *
+ * @param {Array} array
+ * @param {Number} itemsPerLine MAX 5
+ */
+function prettyArrays(array, itemsPerLine){
+    let newArray = [];
+    let POS = {
+        count: 0,
+        place: 0
+    };
+
+    for (let x = 0; x < array.length; x++) {
+        let cmd = array[x];
+
+        if (!newArray[POS.place]) newArray[POS.place] = [];
+        newArray[POS.place].push(cmd);
+
+        POS.count++;
+        if (POS.count == itemsPerLine) {
+            POS.count = 0;
+            POS.place++;
+        };
+    };
+
+    return newArray;
 };
