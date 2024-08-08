c3a767b99828f76ca129a8b61ec004e2dcdd4827
§π∆d°≈~§¶¥#1904 <29105063+ScionSpy@users.noreply.github.com>
2024-08-08 18:37:16 -0400

    addedTimeformating utility

--- Src/Discord/Helpers/Utils.js
+++ Src/Discord/Helpers/Utils.js
@@ -32,6 +32,23 @@ module.exports = class Utils {
     };
 
     /**
+     * Returns remaining time in days, hours, minutes and seconds
+     * @param {number} timeInSeconds
+     */
+    static timeformat(timeInSeconds) {
+        const days = Math.floor((timeInSeconds % 31536000) / 86400);
+        const hours = Math.floor((timeInSeconds % 86400) / 3600);
+        const minutes = Math.floor((timeInSeconds % 3600) / 60);
+        const seconds = Math.round(timeInSeconds % 60);
+        return (
+            (days > 0 ? `${days} days, ` : "") +
+            (hours > 0 ? `${hours} hours, ` : "") +
+            (minutes > 0 ? `${minutes} minutes, ` : "") +
+            (seconds > 0 ? `${seconds} seconds` : "")
+        );
+    }
+
+    /**
      * @param {import("discord.js").PermissionResolvable[]} perms
      */
     static parsePermissions(perms) {
