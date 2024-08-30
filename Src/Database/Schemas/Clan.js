916c2fc9e3dd784c6e7dcb178723d3cf63ac210c
§π∆d°≈~§¶¥#1904 <29105063+ScionSpy@users.noreply.github.com>
2024-08-30 13:33:37 -0400

    Minor change

--- Src/Database/Schemas/Clan.js
+++ Src/Database/Schemas/Clan.js
@@ -40,7 +40,7 @@ module.exports = class Clan {
      */
     async getInviteFor(data){
         if (!data || typeof data !== "object") throw new Error(`Clan.getInviteFor(data); 'data' must be defined and an object. got ${typeof data}`);
-        if ((!data.name && !data.id)) throw new Error(`Clan.getInvitesFor(data); 'data' must have one of either 'data.name' as a String or 'data.id' as a Number. Got Neither.`);
+        if (!data.name && !data.id) throw new Error(`Clan.getInvitesFor(data); 'data' must have one of either 'data.name' as a String or 'data.id' as a Number. Got ${JSON.stringify(data, null, 4)}.`);
         if (data.name && typeof data.name !== "string") throw new Error(`Clan.getInviteFor(data); 'data.name' must be a string! Got ${typeof data.name}`);
         if (data.id && typeof data.name !== "number") throw new Error(`Clan.getInviteFor(data); 'data.id' must be a Number! got ${typeof data.id}`);
 
