a99d1231afc05e1b1482110cce985e1ecfe055f2
§π∆d°≈~§¶¥#1904 <29105063+ScionSpy@users.noreply.github.com>
2024-09-08 23:45:33 -0400

    Update Database.Player.Create

--- Src/Database/Schemas/Player/Player.js
+++ Src/Database/Schemas/Player/Player.js
@@ -18,8 +18,89 @@ module.exports = class Player extends Database {
         this.needsLoading = true;
     };
 
+    /**
+     *
+     * @param {import('../../../WebAPI/Wargaming/Calls/ClanData.js').Clan_Member} player Member joining the Clan.
+     * @param {import('../../../WebAPI/Wargaming/Calls/ClanData.js').InviteData} inviteData invite the member joined with.
+     */
+    async create(player, inviteData){
+        if (typeof player !== "object") throw new Error(`Database.Player.create(player, inviteData); 'player' must be an object! got ${typeof player} : ${player}`);
+        if (typeof inviteData !== "object") throw new Error(`Database.Player.create(player, inviteData); 'inviteData' must be an object! got ${typeof inviteData} : ${inviteData}`);
+
+        let existingData = {
+            memberData: await this._Get("Member", {id:player.account_id}),
+            verifiedData: await this._Get("Verified", { id: player.account_id }),
+            adminData: await this._Get("Admin", { id: player.account_id }),
+        };
+
+        console.log(existingData)
+
+        if(
+            existingData.memberData ||
+            existingData.verifiedData ||
+            existingData.adminData
+        ){
+            throw new Error(`Database.Player.create(player, inviteData); Attempted to create data for existing ID ${player.account_id}\n> Player : ${JSON.stringify(player, null, 4)}\n> inviteData : ${JSON.stringify(inviteData, null, 4)}`);
+        };
+
+        let memberData = {
+            active: true,
+            id: player.account_id,
+            name: player.account_name,
+            inviter: {
+                id: inviteData.sender.id,
+                name: inviteData.sender.name,
+            },
+            clan_role: player.role,
+            clan_joined: player.joined_at,
+            clan_left: null,
+            duration: null,
+            last_logOut: null,
+            last_battle: null,
+            battles: null,
+        };
+
+        if(!inviteData.is_hidden_statistics){
+            memberData.battles = inviteData.statistics.btl;
+        };
+
+        let adminData = {
+            id: player.account_id,
+            discord: {
+                exempt: false,
+                dmLocked: null
+            },
+            inactive: null,
+            loa: null,
+            notes: null
+        };
+
+        await this._Post("Members", memberData);
+        await this._Post("Admin", adminData);
+
+        this.id = memberData.id;
+        this.name = memberData.name;
+        this.discord_id = null;
+        this.clan = {};
+        this.clan.id = inviteData.sender.clan_id;
+        this.clan.tag = "G-C-A";
+        this.clan.rank = player.role;
+        this.clan.active = true
+
+        this.stats = {};
+        this.stats.inactive = false;
+        this.stats.loa = false;
+        this.stats.joined = player.joined_at;
+        this.stats.lastLogOut = null;
+        this.stats.lastBattle = null;
+        this.stats.battles = memberData?.battles || -1;
+
+        return this;
+    };
+
     async load(){
         let query;
+        console.log(this)
         if(this.id) query = {id:this.id};
         else query = {name:this.name};
 
@@ -28,14 +109,19 @@ module.exports = class Player extends Database {
         let admin = await this._Get("Admin", query) || null;
 
         if(verifiedUser){
-            this.id = verifiedUser[0].id;
-            this.name = verifiedUser[0].name;
-            this.discord_id = verifiedUser[0].discord_id;
+            this.id = verifiedUser[0]?.id;
+            this.name = verifiedUser[0]?.name;
+            this.discord_id = verifiedUser[0]?.discord_id;
 
-        }else{
-            this.id = clanMember[0].id;
-            this.name = clanMember[0].name;
-            this.discord_id = clanMember[0].discord_id;
+        }else if(clanMember){
+            this.id = clanMember[0]?.id;
+            this.name = clanMember[0]?.name;
+            this.discord_id = null;
+
+        } else {
+            this.id = this.id;
+            this.name = this.name;
+            this.discord_id = null;
         };
 
         if(clanMember){
@@ -99,45 +185,92 @@ module.exports = class Player extends Database {
     };
 
 
-    async setLoA(author = 'G-C-A\'s Client', reason, exempt = false){
+    async setLoA(author = 'G-C-A\'s Client', reason) {
+        if (typeof author !== "string") throw new Error(`Player.setLoA(author, reason, exempt = false); {author} must be a string! got ${typeof author} : ${author}`);
+        if (!reason || typeof reason !== "string") throw new Error(`Player.setLoA(author, reason, exempt = false); {reason} must be defined as a string! got ${typeof reason}${reason ? ' : ' + reason : ''}`);
         let res;
 
         if (reason == "null" || reason == "clear") {
             res = await this._Edit("Members", { id: this.id }, { isLoA: false });
-            res = await this._Edit("Admin", {id:this.id}, {loa: null});
+            let loa = await this._Get("Admin", {id: this.id}, {loa:1});
+            if(!loa) throw new Error(`Player.setLoA('clear'); Player is not actively on LoA!`);
+
+            loa = loa[0];
+            if (!loa.history) loa.history = [];
+
+            loa.history.push({
+                start: loa.active.start,
+                end: Date.now(),
+                reason: loa.active.reason,
+                authedBy: loa.active.authedBy
+            });
+            loa.active = null;
+
+            res = await this._Edit("Admin", { id: this.id }, { loa });
         }else{
             let loa = {
-                auth: author,
                 start: Date.now(),
-                exempt,
-                reason
+                end: null,
+                reason,
+                authedBy: author
             };
 
             await this._Edit("Members", { id: this.id }, { isLoA:true})
-            res = await this._Edit("Admin", {id:this.id}, {loa});
+            res = await this._Edit("Admin", {id:this.id}, {'loa.active': loa});
         };
         if (res.modifiedCount > 0) res = true;
         else res = false;
         return res;
     };
 
+    async setLoaExempt(exempt = false){
+        if (typeof exempt !== "boolean") throw new Error(`Player.setLoA(author, reason, exmept = false); {exempt} must be a boolean! got ${typeof exempt} : ${exempt}`);
+        let res;
+        if (this.admin.loa.exempt != exempt) res = await this._Edit("Admin", {id:this.id}, {"loa.exempt": exempt});
+        if(!res) throw new Error(`Player.setLoaExempt(exempt); Failed to modify database entry!\n> ${JSON.stringify(res, null, 4)}`);
 
-    async setInactive(author = 'G-C-A\'s Client', reason) {
-        if(reason !== 'set' && reason !== 'clear') throw new Error(`Player.setInactive(author, reason); {reason} must be set to 'set' or 'clear'! got ${reason}`);
+        return { loa: exempt };
+    };
+
+
+    /**
+     *
+     * @param {String} type setInactive sub command.
+     * * 'set' or 'clear'
+     * @param {Number} [start] Date inactivity started. (Default: Date.now() / 1000)
+     * * Paramter should be date since epoch in seconds.
+     * @returns
+     */
+    async setInactive(type, start = Date.now()/1000) {
+        if (type !== 'set' && type !== 'clear') throw new Error(`Player.setInactive(type); {type} must be set to 'set' or 'clear'! got ${type}`);
         let res;
 
-        if (reason == "clear") {
-            res = await this._Edit("Members", { id: this.id }, { isInactive: false });
-            res = await this._Edit("Admin", { id: this.id }, { isInactive: null });
+        let inactivity = await this._Get("Admin", { id: this.id }, { inactive: 1 });
 
-        } else if(reason == 'set') {
-            let inactive = {
-                auth: author,
-                start: Date.now()
-            };
+        if (type == "clear") {
+            res = await this._Edit("Members", { id: this.id }, { isInactive: false });
+            if (!inactivity) throw new Error(`Player.setInactive('clear'); Player is not inactive!`);
+
+            inactivity = inactivity[0];
+            if (!inactivity.history) inactivity.history = [];
+
+            inactivity.history.push({
+                start: inactivity.since,
+                end: Date.now(),
+                duration: (Date.now() - inactivity.since)
+            });
+            inactivity.duration += inactivity.history[0].duration;
+            inactivity.since = null;
+
+            res = await this._Edit("Admin", { id: this.id }, { inactive: inactivity });
+
+        } else if (type == 'set') {
+            if (!inactivity) inactivity = {};
+
+            inactivity.since = start;
 
             res = await this._Edit("Members", { id: this.id }, { isInactive: true });
-            res = await this._Edit("Admin", { id: this.id }, { inactive });
+            res = await this._Edit("Admin", { id: this.id }, { inactive: inactivity });
         };
         if (res.modifiedCount > 0) res = true;
         else res = false;
@@ -149,13 +282,11 @@ module.exports = class Player extends Database {
         let res = {
             members: null,
             verified: null,
-            admin: null
         };
 
         this.name = name;
         res.members = await this._Edit("Members", { id: this.id }, { name });
         if(this.discord_id) res.verified = await this._Edit("Verified", { id: this.id }, { name });
-        res.verified = await this._Edit("Admin", { id: this.id }, { name });
 
         return res;
     };
