01b5865490e9c73149033d554b4d8ac68bb5b6ff
§π∆d°≈~§¶¥#1904 <29105063+ScionSpy@users.noreply.github.com>
2024-09-19 13:35:50 -0400

    ClanBattles: Added map images

---
+++ Src/Modules/ClanBattles/Maps/index.js
@@ -0,0 +1,33 @@
+const pathToMaps = __dirname;
+
+module.exports = {
+    //High-Res Map images.
+    //  "": './highRes/xxx.jpg',
+    "crash_zone_alpha": pathToMaps + '/highRes/Crash_Zone_Alpha.jpg',
+    "hotspot": pathToMaps + '/highRes/Hotspot.jpg',
+    "haven": pathToMaps + '/highRes/Haven.jpg',
+    "islands_of_ice": pathToMaps + '/highRes/Islands_of_Ice.jpg',
+    "land_of_fire": pathToMaps + '/highRes/Land_of_Fire.jpg',
+    "loop": pathToMaps + '/highRes/Loop.jpg',
+    "new_dawn": pathToMaps + '/highRes/New_Dawn.jpg',
+    "north": pathToMaps + '/highRes/North.jpg',
+    "northern_waters": pathToMaps + '/highRes/Northern_Waters.jpg',
+    "sea_of_fortune": pathToMaps + '/highRes/Sea_of_Fortune.jpg',
+    "shards": pathToMaps + '/highRes/Shards.jpg',
+    "shatter": pathToMaps + '/highRes/Shatter.jpg',
+    "sleeping_giant": pathToMaps + '/highRes/Sleeping_Giant.jpg',
+    "tears_of_the_desert": pathToMaps + '/highRes/Tears_of_the_Desert.jpg',
+    "the_atlantic": pathToMaps + '/highRes/The_Atlantic.jpg',
+    "the_faroe_islands": pathToMaps + '/highRes/The_Faroe_Islands.jpg',
+    "trap": pathToMaps + '/highRes/Trap.jpg',
+    "trident": pathToMaps + '/highRes/Trident.jpg',
+    "warriors_path": pathToMaps + '/highRes/Warriors_Path.jpg',
+
+
+    //Low-Res Map images.
+    //  "": './lowRes/xxx.png',
+
+    //Low-Res Maps (Unknown name)
+    "gzttmw3": pathToMaps + '/lowRes/gZTtmW3.png',
+    "yh5tier": pathToMaps + '/lowRes/yh5Tier.png',
+};
